8f55df0a1e88d4c21ac6a0cc8d061deed9c2b541
Saurav Adhikari <sauravads123@gmail.com>
2020-07-29 14:23:49 +0545

    Implemented skaffold for running containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PostCreate from "./components/PostCreate"
 import PostList from "./components/PostList"
-export default () => (< div className="container">
-    <h1>Create Post</h1>
-    <PostCreate />
-    <hr />
-    <h2>Recent Post</h2>
-    <PostList />
-</div >);
+export default () => (
+    <div className="container">
+        <h1> Create Post! </h1>
+        <PostCreate />
+        <hr />
+        <h2> Recent Post </h2>
+        <PostList />
+    </div>
+);
